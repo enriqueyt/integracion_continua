480bd2398deb36aba00ea1809b4b0c33e5f2f83f
Jose Ortiz <40464262+joseop1105@users.noreply.github.com>
2023-06-18 11:25:14 -0500

    JFOT - Ajuste de codigo tema 2

--- deploy/clean.sh
+++ deploy/clean.sh
@@ -12,3 +12,5 @@ docker network rm frontend_backend_network
 # Eliminar la carpeta para la persistencia de datos de Elastic
 echo "Eliminando la carpeta de persistencia de datos..."
 rm -rf ./database-data
+
+echo "Ajuste de codigo fuente JFOT ejercicio 2"
